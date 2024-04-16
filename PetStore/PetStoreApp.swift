d2fbc14432dad2f9295070f1f1ff85d9f708e3f6
Nibha Maharjan <nibhamaharjan@yahoo.com>
2024-04-15 21:56:35 -0400

    Initial Commit

---
+++ PetStore/PetStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetStoreApp.swift
+//  PetStore
+//
+//  Created by Nibha Maharjan on 2024-04-15.
+//
+
+import SwiftUI
+
+@main
+struct PetStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
